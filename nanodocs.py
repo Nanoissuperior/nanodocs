43c74b082385e4e73fd5429ad5af559620807ce2
root <root@vmi297896.contaboserver.net>
2019-11-20 11:17:28 +0100

    Initial work for glossary

--- nanodocs.py
+++ nanodocs.py
@@ -16,13 +16,13 @@ from discord.utils import get
 from dotenv import load_dotenv
 
 load_dotenv()
-
-TOKEN = os.getenv('TOKEN', None)
+TOKEN="NjQ2MzU0MjY4MDEzNTkyNTg2.XdP7Mw.s8L4otbWhW8sBI4IEkCiVsyAofU"
 if TOKEN is None:
     print('Set TOKEN="YOUR_TOKEN" in the .env file')
     sys.exit(1)
 
-game = discord.Game("RPC COMMANDS")
+
+game = discord.Game("BROWSING NANO DOCS")
 
 description = '''NanoDocs BOT - Will send info upon request'''
 bot = commands.Bot(command_prefix='#', description=description)
@@ -104,6 +104,69 @@ def loadRPCdescr():
     cache['rpc'] = rpc_list
     return rpc_list
 
+def loadGLOSSdescr():
+    try:
+        return cache['glossary']
+    except KeyError:
+        pass
+    url = "https://docs.nano.org/glossary/"
+    print('Refreshing cache from ', url)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, features='lxml')
+
+    gloss_list = dict({})
+
+    for irrelevant in soup.find_all('div', attrs={'class': 'codehilite'}):
+        irrelevant.extract()
+
+    for strong in soup.find_all('strong'):
+        strong.replaceWith('**' + strong.get_text() + '**')
+
+    for italic in soup.find_all('em'):
+        italic.replaceWith('*' + italic.get_text() + '*')
+
+    for code in soup.find_all('code'):
+        code.replaceWith('`' + code.get_text() + '`')
+
+    for h4 in soup.find_all('h4'):
+        # Command name is the id of the h3
+        command = h4.get('id')
+
+        # The description is after the h3
+        next_p = h4.find_next_sibling()
+        fields = []
+        descr = ''
+        while next_p is not None and next_p.name != 'h4':
+            if next_p.has_attr('class'):
+                cl = next_p.get('class')
+            else:
+                cl = ''
+            text = next_p.get_text()
+            if len(text) > 1 and text[:2] == '**':
+                break
+
+            if 'admonition' in cl:
+                fields += [(cl[1].upper(), next_p.get_text().strip().replace('  ', ' '))]
+            else:
+                descr += next_p.get_text().strip().replace('  ', ' ') + '\n'
+            next_p = next_p.find_next_sibling()
+
+        descr = descr.rstrip('\n\r')
+        # Adding the command description to the list
+        e = discord.Embed(title=command, description=descr, url=url+'#'+command, color=0x00a0ea)
+        e.set_author(name="Nano Glossary", url=url,
+                     icon_url='https://docs.nano.org/images/favicon.png')
+        e.set_thumbnail(url='https://cdn.discordapp.com/avatars/636508303031271444/8a0cc1a53cdc48b726dcb999f707a90d.png?size=1024')
+
+        for (n, v) in fields:
+            e.add_field(name=n, value=v)
+        gloss_list[command] = e
+    cache['glossary'] = gloss_list
+    return gloss_list
+
+
+
+
 
 @bot.group()
 async def rpc(ctx):
@@ -122,6 +185,24 @@ async def rpc(ctx):
         except:
             await ctx.send("Hmm, I can't find that, maybe look at https://docs.nano.org/commands/rpc-protocol/")
 
+@bot.group(aliases=['gloss','explain'])
+async def glossary(ctx):
+    try:
+        gloss_list = loadGLOSSdescr()
+    except Exception as e:
+        print('Error when getting Gloss: ', e)
+
+    print(ctx.subcommand_passed)
+    if ctx.subcommand_passed is None:
+        await ctx.send("Please specify what you're looking for.")
+
+    else:
+        try:
+            await ctx.send(embed=gloss_list[ctx.subcommand_passed])
+        except:
+            await ctx.send("Hmm, I can't find that, maybe look at https://docs.nano.org/glossary/")
+
+
 
 @bot.event
 async def on_message(message):
@@ -136,7 +217,17 @@ async def on_message(message):
         rpc = parse.fragment.split(' ')[0]
         if rpc in rpc_list:
             await message.channel.send(embed=rpc_list[rpc])
+    try:
+        gloss_list = loadGLOSSdescr()
+    except Exception as e:
+        print('Error when getting Gloss: ', e)
 
+    base_url = "https://docs.nano.org/glossary/"
+    parse = urllib.parse.urlparse(message.content)
+    if base_url in parse.path:
+        gloss = parse.fragment.split(' ')[0]
+        if gloss in gloss_list:
+            await message.channel.send(embed=gloss_list[glossary])
     await  bot.process_commands(message)
 
 if __name__ == "__main__":
@@ -146,3 +237,7 @@ if __name__ == "__main__":
         pass
     except Exception as e:
         print(e)
+
+
+
+
